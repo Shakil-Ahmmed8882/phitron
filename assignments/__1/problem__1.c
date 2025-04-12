dd6857860b595a18e207f1c8987e019b0fe62f80
shakil8882 <shakilahmmed8882@gmail.com>
2025-04-12 12:30:12 +0600

    assignment-1: C programming: problem___ 1

---
+++ assignments/__1/problem__1.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+/**
+     * @brief Printing Predefined Lines to the Console.
+     *
+     * This program directly prints a specific set of lines to the standard
+     * output. The lines include a greeting, a statement about learning C,
+     * emoticons, an affirmation about programming, a repeating pattern,
+     * a declaration of dedication, and a concluding statement of aspiration.
+     * Notably, the last line contains a tab character before "I will succeed".
+     *
+     * Input: This program takes no input from the user.
+     *
+     * Output:
+     * Hello, world! I am learning C programming language. ^_^
+     * Programming is fun and challenging. /\/\/\
+     * I want to give my 100% dedication to learn!	I will succeed one day.
+     */
+
+int main(){
+
+    printf("Hello, world! I am learning C programming language. ^_^\n");
+    printf("Programming is fun and challenging. /\\/\\/\\\n");
+    printf("I want to give my 100%% dedication to learn!\t\tI will succeed one day.\n");
+}
