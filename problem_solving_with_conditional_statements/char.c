3344ce9c04bd5d291f37305e64fa01e81455139e
shakil8882 <shakilahmmed8882@gmail.com>
2025-04-14 20:19:30 +0600

    problem_4:Lower letter to upper, vice versa (ASCI)

---
+++ problem_solving_with_conditional_statements/char.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Problem: Given a letter X.
+// Description: If the letter is lowercase print the letter after converting it from
+//              lowercase letter to uppercase letter. Otherwise print the letter after
+//              converting it from uppercase letter to lowercase letter.
+// Note : difference between 'a' and 'A' in ASCII is 32 .
+//
+// Input:
+// Only one line containing a character X which will be a capital or small letter.
+//
+// Output:
+// Print the answer to this problem.
+//
+// Examples:
+// Input:
+// a
+// Output:
+// A
+//
+// Input:
+// A
+// Output:
+// a
+int main()
+{
+    
+    char ch; 
+    scanf("%c", &ch);
+
+    if(ch >= 'a' && ch <= 'z'){
+        ch = ch - 32; 
+        printf("%c", ch);
+    }else{
+        ch = ch + 32; 
+        printf("%c", ch);
+    }
+
+    return 0;
+}
