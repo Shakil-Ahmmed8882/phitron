ffb41b118c0546f5e6ec09d493dd4c5673a16480
shakil8882 <shakilahmmed8882@gmail.com>
2025-04-19 18:55:31 +0600

    module_7:array declaration & initialization (C)

---
+++ module_7/array_initialization.c
@@ -0,0 +1,27 @@
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int x; // declare 
+    x = 10; //  initialization 
+    int y = 20; // declare and initialization
+
+    int arr[] = {1,2,3,4, 200};
+    int arrOfZero[5] = {0};
+
+
+    // for(int i = 0; i < 5; i++){
+    //     printf("%d ", arr[i]);
+    // }
+
+    for(int i = 0; i < 5; i++){
+        printf("%d ", arrOfZero[i]);
+    }
+
+    
+
+    return 0;
+}
