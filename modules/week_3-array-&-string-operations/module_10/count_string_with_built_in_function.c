c7ced199cb639f7f7c1dfe69fb0c1da10e6a04eb
shakil8882 <shakilahmmed8882@gmail.com>
2025-04-24 18:17:59 +0600

    B_Let_s_use_Getline

---
+++ modules/week_3-array-&-string-operations/module_10/count_string_with_built_in_function.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[100]; 
+    scanf("%s", &s);
+    int stringSize = strlen(s);
+    printf("%d", stringSize);
+
+
+    return 0;
+}
