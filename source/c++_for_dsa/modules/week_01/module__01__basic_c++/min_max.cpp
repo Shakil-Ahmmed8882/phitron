632a8ea1bdb63ebfa56cb8572ae4fa78fcb4a9b2
unknown <shakilahmmed8882@gmail.com>
2025-06-02 17:15:21 +0600

    C++: min , max

---
+++ source/c++_for_dsa/modules/week_01/module__01__basic_c++/min_max.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    int b;
+
+    cin >> a >> b;
+    // cout << min(a, b);
+    cout << max(a, b);
+
+
+    return 0;
+}
