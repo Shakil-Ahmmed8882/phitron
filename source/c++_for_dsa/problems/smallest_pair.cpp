8bc297f65c1486a928d8603fca6be7ff2a9637fa
unknown <shakilahmmed8882@gmail.com>
2025-06-06 11:08:28 +0600

    C++(problem): Smallest pair

---
+++ source/c++_for_dsa/problems/smallest_pair.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        int min_sum = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int current = a[i] + a[j] + (j - i);
+                if (current < min_sum) {
+                    min_sum = current;
+                }
+            }
+        }
+
+        cout << min_sum << endl;
+    }
+
+    return 0;
+}
