baa4919420a7fd20c8522a66a1eb557858e35004
unknown <shakilahmmed8882@gmail.com>
2025-06-06 19:45:56 +0600

    C++: built in usefull functions

---
+++ source/c++_for_dsa/modules/week_02/module_05-string-class-c++/string_modifiers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1 = "Welcome";
+    string s2 = " wave your handds.";
+    // string s3 = s1 += s2; 
+    // string s3 = s1.append(s2); 
+    //  s1.push_back('A'); 
+    // s1+= " Ball"; 
+    //  s1.pop_back(); 
+    //   s1.assign("Subscribe");
+    // s1 = s2; 
+    // s1.erase(3,3);
+    // s1.replace(2,0,"nothing");
+    // s1.insert(2,0,"nothing");
+
+    
+    cout << s1 << endl; 
+
+    return 0;
+}
