b5ab6d5b7d89b9ba60d560da9fe6274377d830a7
unknown <shakilahmmed8882@gmail.com>
2025-06-06 14:18:43 +0600

    C++: find who got the highest mark using class & object on multiple test cases

---
+++ source/c++_for_dsa/assignments/__1/monkey.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    char s[100001];
+
+    while (cin.getline(s, 100001)) {
+        int j = 0;
+        // Remove spaces
+        for (int i = 0; s[i] != '\0'; i++) {
+            if (s[i] != ' ') {
+                s[j++] = s[i];
+            }
+        }
+        s[j] = '\0'; 
+
+        sort(s, s + j); 
+
+        cout << s << endl;
+    }
+
+    return 0;
+}
