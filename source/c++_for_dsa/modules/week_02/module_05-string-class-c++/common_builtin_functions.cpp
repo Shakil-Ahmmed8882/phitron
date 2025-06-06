ba3da397d853e84ed18cd29954fe989394c92778
unknown <shakilahmmed8882@gmail.com>
2025-06-06 18:58:49 +0600

    C++: built in usefull functions

---
+++ source/c++_for_dsa/modules/week_02/module_05-string-class-c++/common_builtin_functions.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s = "Hello world. Here I am.";
+    // cout << s.size() << endl;
+    // cout << s.max_size() << endl;
+    // cout << s.capacity() << endl;
+
+    
+    // s.clear(); 
+    // cout << s.size() << endl;
+    // s.clear(); 
+    // if(s.empty()){
+    //     cout << "Empty" << endl; 
+    // }else{
+    //     cout << "Not Empty" << endl; 
+    // }
+
+    // s.resize(100);
+    cout << s << endl; 
+
+
+
+    return 0;
+}
