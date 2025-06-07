209ea46e3bd2b7372eed9425e1f11787e6268b18
unknown <shakilahmmed8882@gmail.com>
2025-06-07 09:12:17 +0600

    C++: String constructor

---
+++ source/c++_for_dsa/modules/week_02/module_06-more-about-class-and-string/string_constructor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s = "hello world";  // hello world 
+    // string s("Hi world"); // hi world 
+    // string s("Hi world"); // hello world 
+    // string ss(s, 4); // orld
+    // string ss(2, 'x'); // xx
+
+
+
+
+    // cout << ss << endl; 
+
+    return 0;
+}
