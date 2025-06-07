8b85dd2e31fb7524938d4bdaeb4180f9631393dc
unknown <shakilahmmed8882@gmail.com>
2025-06-07 11:18:11 +0600

    C++: reverse word

--- source/c++_for_dsa/modules/week_02/module_06-more-about-class-and-string/range_based_loop.cpp
+++ source/c++_for_dsa/modules/week_02/module_06-more-about-class-and-string/range_based_loop.cpp
@@ -2,6 +2,40 @@
 using namespace std;
 int main()
 {
+    string s; 
+    getline(cin, s) ; 
+    int n; 
+    cin >> n; 
+    int a[n]; 
+
+    for(int i = 0; i  < n ; i++){
+        cin >> a[i]; 
+    }
     
+    
+    // reverse(a, a + n); 
+for(int i = 0; i < n / 2; i++) {
+    int temp = a[i];
+    a[i] = a[n - 1 - i];
+    a[n - 1 - i] = temp;
+}
+
+
+    for(int e: a){
+        cout << e << " "; 
+    }
+
+
+    // for(int i = 0; i < s.size(); i++){
+    //     cout << s[i] << endl; 
+    // }
+    // for(char c: s){
+    //     cout << c << endl; 
+    // }
+
+    // reverse(s.begin(), s.end()); 
+    // cout << s << endl; 
+
+
     return 0;
 }
