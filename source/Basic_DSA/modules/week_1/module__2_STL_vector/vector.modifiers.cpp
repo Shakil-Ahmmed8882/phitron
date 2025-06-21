2ecf9e0221a827ec156e0c014949f50cc4118428
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-06-21 20:18:07 +0600

    DSA: Vector element modifiers

---
+++ source/Basic_DSA/modules/week_1/module__2_STL_vector/vector.modifiers.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> v = {1,2,3};
+    // vector<int> v2 = v;
+    // v2.insert(v2.begin() + 2, 100);
+    vector<int> v1 = {1, 10, 3, 10, 4, 5, 6, 7, 10};
+    vector<int> v2 = {100, 200, 300};
+    // 100, 1, 2, 3, 200, 300
+    // v2.insert(v2.begin() + 1,  v1.begin(), v1.end());
+    // v2.pop_back();
+    // v1.erase(v1.begin());
+    // v1.erase(v1.begin() + 1, v1.begin() + 6);
+    // replace(v1.begin(), v1.begin() + 4, 10, 88);
+
+    // auto it = find(v1.begin(), v1.end(), 10);
+
+
+    // if(it ==v1.end()){
+    //     cout << "Not found" ;
+    // }else{
+    //     cout << "found" ;
+    // }
+
+    
+
+    
+
+    return 0;
+}
