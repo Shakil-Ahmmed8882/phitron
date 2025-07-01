ae4155c7d9426c8c61c8d09e916d6ffb55b2b599
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-07-01 18:01:58 +0600

    DSA-Linked-List: optimize  o(n*n) complexity of tail insert into o(1)

--- source/Basic_DSA/modules/week_2/singly_linked_list_operations/insert_at_tail.cpp
+++ source/Basic_DSA/modules/week_2/singly_linked_list_operations/insert_at_tail.cpp
@@ -15,7 +15,7 @@ public:
     }
 };
 
-void insert_at_tail(Node *&head, int val)
+void insert_at_tail(Node *&head, Node *&tail, int val)
 {
     Node *newNode = new Node(val);
     if (head == NULL)
@@ -23,13 +23,8 @@ void insert_at_tail(Node *&head, int val)
         head = newNode;
         return;
     }
-
-    Node *temp = head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    } // now temp is in the last node
-    temp->next = newNode;
+    tail->next = newNode;
+    tail = tail->next;
 };
 
 void print_linked_list(Node *head)
@@ -45,14 +40,15 @@ void print_linked_list(Node *head)
 int main()
 {
 
-    // Node *head = new Node(10);
-    // Node *a = new Node(20);
-    // head->next = a;
-    Node *head = NULL;
-    insert_at_tail(head, 200);
-    insert_at_tail(head, 300);
-    insert_at_tail(head, 400);
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+    head->next = a;
+    a->next = tail;
+    insert_at_tail(head, tail, 200);
+    insert_at_tail(head, tail, 500);
     print_linked_list(head);
+    cout << "tail : " << tail->val << endl;
 
     return 0;
 }
