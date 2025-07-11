9997822e04b5fb455e7478e181fda839ca78852b
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-07-11 20:24:29 +0600

    DSA-Linked-List-STL: essential functions: find, replace, erase, resize, assign

--- source/Basic_DSA/modules/week_3/stl_list_and_cycle_detection/stl_list.cpp
+++ source/Basic_DSA/modules/week_3/stl_list_and_cycle_detection/stl_list.cpp
@@ -18,12 +18,24 @@ int main()
     // list<int> l2(l1);
     // int ar[] = {1, 2, 3, 4};
     // list<int> l2(ar, ar + 4);
-    vector<int> v= {100, 200, 300, 400};
+    vector<int> v = {8, 8, 300, 8, 400};
     list<int> l2(v.begin(), v.end());
-    for (int v : l2)
-    {
-        cout << v << endl;
-    }
+
+    // l2.erase(next(l2.begin(), 1), next(l2.begin(), 3));
+    // replace(l2.begin(), l2.end(), 8, 91);
+    // auto it = find(l2.begin(), l2.end(), 200);
+    // if (it == l2.end())
+    // {
+    //     cout << "not found";
+    // }
+    // else
+    // {
+    //     cout << " found";
+    // }
+    // for (int v : l2)
+    // {
+    //     cout << v << endl;
+    // }
 
     return 0;
 }
