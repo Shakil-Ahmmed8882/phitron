d0a0eb05b49dc92b174d4abb85b02eae53f84b03
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-07-11 20:42:37 +0600

    DSA-Linked-List: List element access and iterators

---
+++ source/Basic_DSA/modules/week_3/stl_list_and_cycle_detection/list_element_access_and_iterators.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    list<int> l = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    // cout << l.front() << endl; 
+    // cout << l.back() << endl; 
+    // cout << *next(l.begin(), 3);
+    cout << *l.begin() << endl;
+    cout << *l.end() << endl;
+
+    return 0;
+}
