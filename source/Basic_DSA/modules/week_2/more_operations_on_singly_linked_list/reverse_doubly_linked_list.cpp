289514b481ed120c614fb3693910b5acce5ad58e
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-07-11 22:53:30 +0600

    DSA-Linked-List: Reverse doubly linked list with 2 pointers

---
+++ source/Basic_DSA/modules/week_2/more_operations_on_singly_linked_list/reverse_doubly_linked_list.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+};
+
+void reverse_doubly_linked_list(Node *head, Node *tail)
+{
+    for (Node *i = head, *j = tail; i != j && i->prev != j; i = i->next, j = j->prev)
+    {
+        swap(i->val, j->val);
+    }
+}
+
+void print(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+
+        insert_at_tail(head, tail, val);
+    }
+    reverse_doubly_linked_list(head, tail);
+    print(head);
+    return 0;
+}
