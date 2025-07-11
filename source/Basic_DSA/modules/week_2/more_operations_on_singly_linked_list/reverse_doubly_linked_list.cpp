db79acbd23696efbba2a5cabd19a503a2ae90f55
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-07-11 23:34:07 +0600

    DSA-Linked-List: Detect cycle

--- source/Basic_DSA/modules/week_2/more_operations_on_singly_linked_list/reverse_doubly_linked_list.cpp
+++ source/Basic_DSA/modules/week_2/more_operations_on_singly_linked_list/reverse_doubly_linked_list.cpp
@@ -15,56 +15,45 @@ public:
     }
 };
 
-void insert_at_tail(Node *&head, Node *&tail, int val)
-{
-    Node *newNode = new Node(val);
-    if (head == NULL)
-    {
-        head = newNode;
-        tail = newNode;
-        return;
-    }
-    tail->next = newNode;
-    newNode->prev = tail;
-    tail = newNode;
-};
-
-void reverse_doubly_linked_list(Node *head, Node *tail)
-{
-    for (Node *i = head, *j = tail; i != j && i->prev != j; i = i->next, j = j->prev)
-    {
-        swap(i->val, j->val);
-    }
-}
-
-void print(Node *head)
-{
-    Node *tmp = head;
-    while (tmp != NULL)
-    {
-        cout << tmp->val << endl;
-        tmp = tmp->next;
-    }
-}
-
 int main()
 {
 
-    Node *head = NULL;
-    Node *tail = NULL;
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
 
-    while (true)
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = head;
+
+    Node *slow = head;
+    Node *fast = head;
+    bool flag = false;
+    while (fast != NULL && fast->next != NULL)
     {
-        int val;
-        cin >> val;
-        if (val == -1)
+
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
         {
+            // cycle detected
+            flag = true;
             break;
         }
-
-        insert_at_tail(head, tail, val);
     }
-    reverse_doubly_linked_list(head, tail);
-    print(head);
+
+    if (flag)
+    {
+        cout << "Cycle Detected" << endl;
+    }
+    else
+    {
+        cout << "No Cycle" << endl;
+    }
+
     return 0;
 }
