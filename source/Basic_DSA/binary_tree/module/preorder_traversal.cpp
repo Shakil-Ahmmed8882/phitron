2bb358fe92db0556b2c1ee86a099bbbc9af3e4eb
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-07-28 06:14:28 +0600

    DSA-Binary-Tree: print following preorder traversal

--- source/Basic_DSA/binary_tree/module/preorder_traversal.cpp
+++ source/Basic_DSA/binary_tree/module/preorder_traversal.cpp
@@ -14,18 +14,25 @@ public:
     }
 };
 
-
+void preorder (Node * root) {
+    if (root == NULL){
+        return; 
+    }
+    cout << root->val << " ";
+    preorder(root->left); 
+    preorder(root->right);
+}
 
 int main()
 {
   
     Node * root = new Node(10);
     Node * a = new Node(20);
-    Node * b = new Node(10);
-    Node * c = new Node(10);
-    Node * d = new Node(10);
-    Node * e = new Node(10);
-    Node * f = new Node(10);
+    Node * b = new Node(30);
+    Node * c = new Node(40);
+    Node * d = new Node(50);
+    Node * e = new Node(60);
+    Node * f = new Node(70);
 
 
     root->left = a; 
@@ -34,7 +41,7 @@ int main()
     b->left=d; 
     b->right = e; 
     e->left = f; 
-
+    preorder(root);
 
 
 
