29a2d2fbf2fc65d4f1379b44fe3db60a32e02b2e
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-08-03 14:06:29 +0600

    DSA-Binary-Tree: Leaf nodes in descending order

---
+++ source/Basic_DSA/binary_tree/module/module_19/Leaf_Nodes.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void take_input(Node *&root)
+{
+
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+
+
+        // extract 
+        Node *p = q.front();
+        q.pop();
+
+        // work on it
+        int l, r;
+        cin >> l >> r;
+        Node *left_node = new Node(l);
+        Node *right_node = new Node(r);
+
+        if (l == -1)
+            p->left = NULL;
+        else
+            p->left = left_node;
+        if (r == -1)
+            p->right = NULL;
+        else
+            p->right = right_node;
+
+        // push the children 
+        if (p->left)
+            q.push(left_node);
+        if (p->right)
+            q.push(right_node);
+    }
+}
+
+
+void print_leaf_sum (Node* root){
+
+    if(!root){
+        return;
+    }
+    queue<Node*> q;
+    vector<int> v; 
+    q.push(root);
+    while(!q.empty()){
+        Node * f = q.front(); 
+        q.pop(); 
+        if(
+            f->left == NULL && f->right == NULL
+        ){
+            v.push_back(f->val); 
+        }
+        if(f->left){
+            q.push(f->left);
+        }
+        if(f->right){
+            q.push(f->right);
+        }
+    }
+
+    sort(v.begin(),v.end());
+    reverse(v.begin(), v.end()); 
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    
+
+}
+
+int main()
+{
+    int v; 
+    cin >> v; 
+    Node * root = new Node(v); 
+    take_input(root);
+    print_leaf_sum(root); 
+
+
+    return 0;
+}
