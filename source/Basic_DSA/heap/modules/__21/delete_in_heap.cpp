b6aaec0ec146785efb84f2d29c383a4ee5bf3962
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-08-05 23:55:45 +0600

    DSA-Heap: Delete in max heap

---
+++ source/Basic_DSA/heap/modules/__21/delete_in_heap.cpp
@@ -0,0 +1,89 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void insert_in_heap(vector<int> &v, int val){
+    v.push_back(val);
+    int current_index = v.size() - 1;
+    while (current_index != 0){
+        int parent_index = (current_index - 1) / 2;
+        if (v[parent_index] < v[current_index]){
+            swap(v[parent_index], v[current_index]);
+        }else{
+            break;
+        }
+        current_index = parent_index;
+    }
+}
+
+
+
+
+void print_value (vector<int>v){
+    for(int i:v)
+        cout << i << " "; 
+}
+
+
+void delete_in_max_heap(vector<int> &v){
+
+    v[0] = v.back(); 
+    v.pop_back(); 
+
+    int cur_idx = 0; 
+    while (true)
+    {
+        
+        int left_idx = cur_idx * 2 + 1; 
+        int right_idx = cur_idx * 2 + 2; 
+
+        int left_val = INT_MIN, right_val = INT_MIN; 
+        if(left_idx < v.size())
+            left_val = v[left_idx]; 
+        if(right_idx < v.size())
+            right_val = v[right_idx]; 
+
+        if(left_val > right_val && left_val > v[cur_idx]){
+            swap(v[left_idx], v[cur_idx]);
+            cur_idx = left_idx; 
+        }
+        else if(right_val > left_val && right_val > v[cur_idx]){
+            swap(v[right_idx], v[cur_idx]); 
+            cur_idx = right_idx; 
+        }
+        else{
+            break; 
+        }
+    }
+    
+
+
+
+    
+}
+
+int main()
+{
+    int n; 
+    cin >> n; 
+    vector<int> v; 
+
+    for(int i = 0; i < n; i++){
+        int val; 
+        cin >> val; 
+        insert_in_heap(v,val); 
+    }
+
+    cout << endl;
+    print_value(v); 
+    cout << endl;
+    delete_in_max_heap(v); 
+    print_value(v); 
+    cout << endl;
+    delete_in_max_heap(v); 
+    print_value(v); 
+
+    return 0;
+}
