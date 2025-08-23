bee88e90cef15313ddaaad527df853e5f3a530c1
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-08-23 23:27:51 +0600

    Algo: revent BFS from traversing blocked cells
    - Added check to skip grid cells marked as '#'
    - Ensured BFS does nothing if the starting cell is blocked
    - This prevents invalid traversal

--- source/algorithm/modules/module_3.5/bfs_in_2d_grid.cpp
+++ source/algorithm/modules/module_3.5/bfs_in_2d_grid.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 char grid[105][105];
 bool vis[105][105];
+int level[105][105];
 vector<pair<int, int>> mv = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 int r, c;
 bool valid(int cr, int cc){
@@ -20,6 +21,7 @@ void bfs(int si, int sj)
     queue<pair<int, int>> q;
     q.push({si, sj});
     vis[si][sj] = true;
+    level[si][sj] = 0; 
     while (!q.empty())
     {
         pair<int, int> par = q.front();
@@ -27,14 +29,14 @@ void bfs(int si, int sj)
         int par_j = par.second;
         q.pop(); 
 
-        cout << par_i << " " << par_j << endl; ; 
         for(int i = 0; i < 4 ; i++){
             int ci = par_i + mv[i].first; 
             int cj = par_j + mv[i].second; 
 
-            if(valid(ci, cj) && !vis[ci][cj]){
+            if(valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.'){
                 q.push({ci, cj}); 
                 vis[ci][cj] = true; 
+                level[ci][cj] = level[par_i][par_j] + 1; 
             }
                 
         }
@@ -53,8 +55,14 @@ int main()
             cin >> grid[i][j];
 
     memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
     int si, sj;
     cin >> si >> sj;
     bfs(si, sj);
+
+    int di, dj; 
+    cin >> di >> dj; 
+    cout << level[di][dj]; 
+
     return 0;
 }
