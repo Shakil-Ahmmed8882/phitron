810574f9dbe1b8a99873f7e883812f46e9b18d56
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-08-23 20:40:38 +0600

    Algo: visualize graph in 2D matrix, track 2d area & print all connection from source in 4 moves

--- source/algorithm/modules/module_3/dfs_on_2d_grid.cpp
+++ source/algorithm/modules/module_3/dfs_on_2d_grid.cpp
@@ -2,37 +2,46 @@
 using namespace std;
 char grid[1005][10005];
 bool vis[1005][10005];
-vector<pair<int, int>> mv = {{-1,0}, {1, 0}, {0, 1}, {0,-1}};
+vector<pair<int, int>> mv = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+int r, c;
 
-void dfs(int si, int sj){
-    // cout << si << " " << sj; 
-    vis[si][sj] = true; 
+bool valid(int cr, int cl)
+{
+    if(cr < 0 || cr >= r || cl < 0 || cl >= c)
+        return false; 
 
-    for(int i = 0; i < 4; i++){
-        int child_row, child_col; 
-        child_row = si +  mv[i].first; 
-        child_col = sj + mv[i].second; 
-
-        cout << child_row << " " << child_col << endl; 
-    }
-
+    return true; 
 
 }
 
+void dfs(int si, int sj)
+{
+    cout << si << " " << sj << endl; 
+    vis[si][sj] = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+
+        int cr = si + mv[i].first; // child row 
+        int cl = sj + mv[i].second; // child column
+
+        if(valid(cr, cl) && !vis[cr][cl])
+            dfs(cr, cl); 
+    }
+}
 
 int main()
 {
-
-    int r, c;
     cin >> r >> c;
 
     for (int i = 0; i < r; i++)
         for (int j = 0; j < c; j++)
             cin >> grid[i][j];
 
-    int si , sj; 
-    cin >> si >> sj; 
-    dfs(si, sj); 
+    int si, sj;
+    cin >> si >> sj;
+    memset(vis, false, sizeof(vis));
+    dfs(si, sj);
 
     return 0;
 }
