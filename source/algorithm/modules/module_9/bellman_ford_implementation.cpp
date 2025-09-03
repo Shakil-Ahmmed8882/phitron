ec0014144d65af79c54cdd054d29a41ced5906ae
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-09-03 18:55:45 +0600

    Algo(Bellman-Ford): Detect negative weighted cycle in directed graph

--- source/algorithm/modules/module_9/bellman_ford_implementation.cpp
+++ source/algorithm/modules/module_9/bellman_ford_implementation.cpp
@@ -35,6 +35,26 @@ void belman_ford()
             }
         }
     }
+    bool cycle = false;
+    for (auto ed : edge_list)
+    {
+        int a, b, c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+
+        if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+            cycle = true;
+    }
+
+    if (cycle)
+        cout << "Cycle Detected" << endl;
+    else
+    {
+
+        for (int i = 0; i < n; i++)
+            cout << i << " -> " << dis[i] << endl;
+    }
 }
 
 int main()
@@ -45,7 +65,7 @@ int main()
         dis[i] = INT_MAX;
 
     dis[0] = 0;
-
+    
     while (e--)
     {
         int a, b, c;
@@ -53,13 +73,7 @@ int main()
         edge_list.push_back(EdgeList(a, b, c));
     }
 
-
     belman_ford();
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << i << " -> " << dis[i] << endl;
-    }
 
     return 0;
 }
