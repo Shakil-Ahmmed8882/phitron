82e9f7d5c1a454ebc03158bca7bd433219a6eae2
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-09-03 18:18:25 +0600

    Algo(Bellman-Ford): theory & implementation

---
+++ source/algorithm/modules/module_9/bellman_ford_implementation.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class EdgeList
+{
+public:
+    int a, b, c;
+    EdgeList(int a, int b, int c)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
+
+int main()
+{
+    int dis[10005];
+    vector<EdgeList> edge_list;
+    int n, e;
+    cin >> n >> e;
+
+
+    for(int i = 0; i < n; i ++)
+        dis[i] = INT_MAX; 
+
+    dis[0] =  0; 
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edge_list.push_back(EdgeList(a, b, c));
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto ed : edge_list)
+        {
+            int a, b, c;
+            a = ed.a;
+            b = ed.b;
+            c = ed.c;
+
+            if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+            {
+                dis[b] = dis[a] + c;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << i << " -> " << dis[i] << endl;
+    }
+
+
+    return 0;
+}
