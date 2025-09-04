8315fcb750fe7bc1040a4b586ca7d2d6dc7e51ba
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-09-04 16:58:33 +0600

    Algo(Floyd-warshall): implement floyd to find multi source cost

---
+++ source/algorithm/modules/module_10/floyd_warshall.cpp
@@ -0,0 +1,56 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_mat[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            adj_mat[i][j] = INT_MAX;
+        }
+    }
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_mat[a][b] = c;
+        // adj[b][a] = c; // undirected
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int new_distance = adj_mat[k][j] + adj_mat[i][k];
+                int is_short_distance = adj_mat[k][j] != INT_MAX && adj_mat[i][k] != INT_MAX &&
+                                        new_distance < adj_mat[i][j]; 
+
+                if (is_short_distance)
+                {
+                    adj_mat[i][j] = new_distance; 
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(adj_mat[i][j] == INT_MAX)
+                cout << "INF" << " ";
+            else
+                cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
