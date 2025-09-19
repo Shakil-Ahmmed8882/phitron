01817c412a316dd0e0600c8c224e3dab7a314713
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-09-19 22:01:09 +0600

    recap(prefix-sum): find range based summation in o(1) operations

---
+++ source/recap-all/STL_vector/Y_Range_sum_query.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+        int n ,q; 
+        cin >> n >> q; 
+        vector<int> v(n + 1); 
+        for(int i = 1; i <= n; i++)
+            cin >> v[i]; 
+
+        
+        vector<int> prefix_sum(n + 1); 
+        prefix_sum[1] = v[1]; 
+
+        for(int i = 2; i <= n; i++){
+            prefix_sum[i] = prefix_sum[i - 1] + v[i]; 
+        }
+
+
+        while (q--)
+        {
+            int l , r; 
+            cin >> l >> r; 
+            int sum = 0; 
+            if(l ==1 )
+                sum = prefix_sum[r]; 
+            else{
+                sum = prefix_sum[r] - prefix_sum[l- 1]; 
+            }
+            cout << sum << endl;    
+
+        }
+        
+
+        
+
+    return 0;
+}
+
