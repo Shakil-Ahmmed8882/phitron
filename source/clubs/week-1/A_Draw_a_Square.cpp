717a440dbf2162d639d6b88aee501f4845118050
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-10-08 19:47:44 +0600

    club(week-1): solve A_Draw_a_Square

---
+++ source/clubs/week-1/A_Draw_a_Square.cpp
@@ -0,0 +1,22 @@
+
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    if (!(cin >> t)) return 0;
+    while (t--) {
+        int l, r, d, u;
+        cin >> l >> r >> d >> u;
+        if (l == r && r == d && d == u) {
+            cout << "Yes\n";
+        }
+        else cout << "No\n";
+    }
+    return 0;
+}
+
