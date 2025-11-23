761ee8c2f8cd5a0866b3d6f7938b9ad5d5365bad
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-11-23 14:37:45 +0600

    club(week_6): solve FAKE GCD https://www.codechef.com/problems/FAKEGCD

---
+++ source/clubs/L_Fake_GCD.cpp
@@ -0,0 +1,37 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
+#define FAST_IO                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+void solve()
+{
+    int N;
+    
+    if (!(cin >> N)) return;
+    for (int i = 1; i <= N; ++i) {
+        
+        cout << i << (i == N ? "" : " ");
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    
+    FAST_IO;
+    int t;
+    
+    if (!(cin >> t)) return 0;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
