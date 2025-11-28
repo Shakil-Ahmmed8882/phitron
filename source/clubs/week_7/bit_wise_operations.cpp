5befcbaa04c7f9b7a2a2b2153ac28c7d948d0854
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-11-28 11:57:59 +0600

    club(week_7): turn on/off & toggle kth bit - ~, ^, &

--- source/clubs/week_7/bit_wise_operations.cpp
+++ source/clubs/week_7/bit_wise_operations.cpp
@@ -23,7 +23,6 @@ int check_kth_bit_on_or_off(int n, int k){
     return ((n >> k )& 1); 
 }
 void print_on_and_off_bits(int n){
-
     for(int k = 7; k >= 0; k--){
         if(check_kth_bit_on_or_off(n, k)){
             cout << "1" << " ";
@@ -32,11 +31,28 @@ void print_on_and_off_bits(int n){
         }
     }
 }
+
+int turn_on_kth_bit(int n, int k){
+    return (n | (1 << k)); 
+}
+
+int turn_off_kth_bit(int n, int k){
+    return (n & ~( 1 << k)); 
+}
+
+int toggle_kth_bit(int n, int k){
+    return ( n ^ ( 1 << k)); 
+}
+
 int main()
 {
     FAST_IO;
     // cout << check_kth_bit_on_or_off(20, 4) << "\n"; 
-    print_on_and_off_bits(20);
+    // print_on_and_off_bits(20);
+    // cout << turn_on_kth_bit(20, 1) << "\n";
+    // cout << turn_off_kth_bit(20, 2) << "\n";
+    cout << toggle_kth_bit(20, 2) << "\n";
+
     
     return 0;
 }
