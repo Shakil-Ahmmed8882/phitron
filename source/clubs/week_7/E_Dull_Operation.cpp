3c3f57c1377f3434ca858650d8ee5fc79cd062e0
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-11-30 10:42:43 +0600

    club(week_7): Dull Operation https://www.codechef.com/problems/DUPLET

---
+++ source/clubs/week_7/E_Dull_Operation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl cout << "\n"
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+
+void solve() {
+    ll N;
+    if (!(cin >> N)) return; 
+    
+    cout << N << " " << N - 1 << "\n";
+}
+
+int main()
+{
+    FAST_IO;
+    
+    int T;
+    if (!(cin >> T)) return 0;
+    
+    while (T--) {
+        solve();
+    }
+    
+    return 0;
+}
