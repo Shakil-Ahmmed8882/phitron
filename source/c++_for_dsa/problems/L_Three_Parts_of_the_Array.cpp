dee678316ccad7fc2ec0428a60987621481cbd6f
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2025-12-12 22:55:51 +0600

    club(week_8): solve Binary search https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/A

---
+++ source/c++_for_dsa/problems/L_Three_Parts_of_the_Array.cpp
@@ -0,0 +1,19 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl cout << "\n"
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+
+int main()
+{
+    FAST_IO;
+
+    int n;
+    cin >> n;
+    cout << "________"<< n <<< nl; 
+    
+    return 0;
+}
